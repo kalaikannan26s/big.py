efe1b75773b82f961a33c6ffc62f98924ee52493
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-06-06 12:35:57 +0530

    Create big2.py

---
+++ big2.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+if a>b and a>c:
+        print("big number")
+elif b>c:
+        print("b is big")
+else:
+        print("c")
